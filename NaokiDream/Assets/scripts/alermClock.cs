b420657a690643eb5a8c07edb4e280660fb45286
HattoriNaoki <toraidennto1323@gmail.com>
2017-05-20 15:42:11 +0900

    test

---
+++ NaokiDream/Assets/scripts/alermClock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alermClock : MonoBehaviour {
+
+    private string datetimeStr;
+    void Update()
+    {
+        datetimeStr = System.DateTime.Now.ToString();
+        Debug.Log(datetimeStr);
+    }
+}
