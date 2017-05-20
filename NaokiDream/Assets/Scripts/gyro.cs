01300bdf23a2144665703389861557674d8f5a1d
kei <fkei120@yahoo.co.jp>
2017-05-20 19:24:04 +0900

    VR、部屋、ベット、ユニティちゃんの実装

---
+++ NaokiDream/Assets/Scripts/gyro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //ジャイロセンサの許可
+        Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //傾き情報を取得
+        Quaternion q = Input.gyro.attitude;
+        Quaternion qq = Quaternion.AngleAxis(-90.0f, Vector3.left);
+        q.x *= -1.0f;
+        q.y *= -1.0f;
+        transform.localRotation = qq * q;
+    }
+}
