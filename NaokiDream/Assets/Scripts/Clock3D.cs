d88e112de7ac8cdb2932549bf4c29fdc5c2dab08
HattoriNaoki <toraidennto1323@gmail.com>
2017-05-21 03:46:37 +0900

    お願いします

---
+++ NaokiDream/Assets/Scripts/Clock3D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Clock3D : MonoBehaviour {
+
+    private string datetimeStr;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        datetimeStr = System.DateTime.Now.ToString();
+
+        var text = gameObject.GetComponent<TextMesh>();
+        //Debug.Log(redatetimeStr.Length);
+        text.text = datetimeStr;
+    }
+}
