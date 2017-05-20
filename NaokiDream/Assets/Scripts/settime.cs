928e07d5af24adc1851f9065d75fec61fffce5e6
HattoriNaoki <toraidennto1323@gmail.com>
2017-05-21 04:38:56 +0900

    セット時間表示

---
+++ NaokiDream/Assets/Scripts/settime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class settime : MonoBehaviour {
+
+    private string saveTime;
+
+    // Use this for initialization
+    void Start () {
+
+        saveTime = SettingTime.getsavetimer();
+
+        if(saveTime == null)
+        {
+            transform.GetComponent<Text>().text = "時間が設定されていません";
+        }
+        else
+        {
+            transform.GetComponent<Text>().text = saveTime;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
